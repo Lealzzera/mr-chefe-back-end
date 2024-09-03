796ba98ad1e0c45df0af0b289113bfd9dd894859
Matheus <matheus.mleall@gmail.com>
2024-09-03 18:49:31 -0300

    feat envs config and installed zod

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { EnvModule } from './env/env.module';
+import { envSchema } from './env-config/env-config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.safeParse(env),
+      isGlobal: true,
+    }),
+    EnvModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
