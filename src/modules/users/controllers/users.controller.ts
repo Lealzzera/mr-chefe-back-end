779d8bfd817d72c12ab8678b81aac734ea86d748
Matheus <matheus.mleall@gmail.com>
2024-09-09 11:22:14 -0300

    feat: adjusted controller to register a new user

--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -1,5 +1,12 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+} from '@nestjs/common';
 import { RegisterService } from '../use-cases/register.service';
+import { z } from 'zod';
 
 class RegisterDto {
   name: string;
@@ -15,7 +22,24 @@ export class UsersController {
   constructor(private readonly registerService: RegisterService) {}
 
   @Post()
+  @HttpCode(201)
   async registerUser(@Body() { name, email, password }: RegisterDto) {
-    const user = await this.registerService.exec({ name, email, password });
+    try {
+      const registerUserBodySchema = z.object({
+        name: z.string(),
+        password: z.string().min(6),
+        email: z.string().email(),
+      });
+
+      const userData = registerUserBodySchema.parse({ name, email, password });
+
+      this.registerService.exec({
+        name: userData.name,
+        email: userData.email,
+        password: userData.password,
+      });
+    } catch (err) {
+      throw new BadRequestException({ message: err.message });
+    }
   }
 }
