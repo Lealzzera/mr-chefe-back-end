ceb4b1dbdf8a8853f0b05863e0fca899c07a2aee
Matheus <matheus.mleall@gmail.com>
2024-09-10 23:15:14 -0300

    implemented and created all users repository methods in a inmemory users repository

---
+++ src/modules/users/repositories/in-memory-repositories/in-memory-users.repository.ts
@@ -0,0 +1,26 @@
+import { User } from '@prisma/client';
+import {
+  CreateUserProps,
+  IUsersRepository,
+} from '../../interfaces/users-repository.interface';
+import { randomUUID } from 'crypto';
+
+export class InMemoryUsersRepository implements IUsersRepository {
+  private usersDatabase = [];
+  async findUsers(): Promise<User[] | []> {
+    const users = this.usersDatabase.map((user) => user);
+    return users;
+  }
+  findByEmail(email: string): Promise<User | null> {
+    const user = this.usersDatabase.find((item) => {
+      return item.email === email;
+    });
+
+    return user ? user : null;
+  }
+  async create({ name, email, password }: CreateUserProps): Promise<User> {
+    const user = { id: randomUUID(), name, email, password };
+    this.usersDatabase.push(user);
+    return user;
+  }
+}
