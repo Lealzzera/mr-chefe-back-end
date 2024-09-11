100bcb22513a1940809436002a2b1b54f225806c
Matheus <matheus.mleall@gmail.com>
2024-09-11 17:12:30 -0300

    removed finduserbyid and find all users service

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,17 +2,12 @@ import { Module } from '@nestjs/common';
 import { UsersRepository } from './repositories/users.repository';
 import { RegisterService } from './use-cases/register.service';
 import { UsersController } from './controllers/users.controller';
-import { FindUserByEmailService } from './use-cases/find-user-by-email.service';
-import { FindAllUsersService } from './use-cases/find-all-users.service';
 
 @Module({
   providers: [
     RegisterService,
-    FindUserByEmailService,
-    FindAllUsersService,
     { provide: 'IUsersRepository', useClass: UsersRepository },
   ],
   controllers: [UsersController],
-  exports: [FindUserByEmailService],
 })
 export class UsersModule {}
