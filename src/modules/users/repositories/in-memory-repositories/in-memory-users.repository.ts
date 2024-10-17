cd4481980e2b38803d8f82b969e376b4a18208e7
Matheus Leal <matheus.mleall@gmail.com>
2024-10-17 17:50:42 -0300

    implemented new in memory methods

--- src/modules/users/repositories/in-memory-repositories/in-memory-users.repository.ts
+++ src/modules/users/repositories/in-memory-repositories/in-memory-users.repository.ts
@@ -1,5 +1,6 @@
-import { User } from '@prisma/client';
+import { User, UserStore } from '@prisma/client';
 import {
+  AddUserToStoreProps,
   CreateUserProps,
   IUsersRepository,
 } from '../../interfaces/users-repository.interface';
@@ -7,6 +8,23 @@ import { randomUUID } from 'crypto';
 
 export class InMemoryUsersRepository implements IUsersRepository {
   private usersDatabase = [];
+
+  async findUserInAstoreById(userId: string): Promise<UserStore | null> {
+    const userStore = await this.usersDatabase.find(
+      (user) => user.userId === userId,
+    );
+    return userStore || null;
+  }
+  async addUserToStore({
+    userId,
+    storeId,
+    role,
+  }: AddUserToStoreProps): Promise<UserStore> {
+    const userStore = { userId, storeId, role };
+    await this.usersDatabase.push(userStore);
+
+    return userStore;
+  }
 
   async findUserById(id: string): Promise<User | null> {
     const user = await this.usersDatabase.find((item) => item.id === id);
@@ -22,8 +40,14 @@ export class InMemoryUsersRepository implements IUsersRepository {
     return user ? user : null;
   }
 
-  async create({ name, email, password }: CreateUserProps): Promise<User> {
-    const user = { id: randomUUID(), name, email, password };
+  async create({
+    name,
+    email,
+    password,
+    phoneNumber,
+    cpf,
+  }: CreateUserProps): Promise<User> {
+    const user = { id: randomUUID(), name, email, password, phoneNumber, cpf };
     await this.usersDatabase.push(user);
     return user;
   }
