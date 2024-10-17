302754dcaae0de8376c1de609b494938e034bbc5
Matheus Leal <matheus.mleall@gmail.com>
2024-10-17 16:39:09 -0300

    Added JWTService to users controller because to add a member it need to be passed by authguard

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { UsersRepository } from './repositories/users.repository';
 import { RegisterService } from './use-cases/register.service';
 import { UsersController } from './controllers/users.controller';
+import { RegisterMemberService } from './use-cases/register-member.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   providers: [
     RegisterService,
+    JwtService,
+    RegisterMemberService,
     { provide: 'IUsersRepository', useClass: UsersRepository },
   ],
   controllers: [UsersController],
